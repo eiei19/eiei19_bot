30dd5d14b04ee8254a3286319a7078ffd2efe989
Akihiro Nagashima <ei.ei.oh.0613@gmail.com>
2016-04-08 13:32:51 +0900

    miss type

--- app/controllers/callback_controller.rb
+++ app/controllers/callback_controller.rb
@@ -5,7 +5,7 @@ class CallbackController < ApplicationController
     hash = OpenSSL::HMAC::digest(OpenSSL::Digest::SHA256.new, channel_secret, http_request_body)
     signature = Base64.strict_encode64(hash)
     p signature
-    p request.heders["X-LINE-CHANNELSIGNATURE"]
+    p request.headers["X-LINE-CHANNELSIGNATURE"]
     render json: [], status: :ok
   end
 end
