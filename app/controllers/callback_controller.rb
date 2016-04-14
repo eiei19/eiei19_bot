53aafc14b8e37444b98139ee449938869e9d5fc0
Akihiro Nagashima <ei.ei.oh.0613@gmail.com>
2016-04-14 14:54:13 +0900

    facebookのエンドポイントを追加

--- app/controllers/callback_controller.rb
+++ app/controllers/callback_controller.rb
@@ -2,6 +2,11 @@ class CallbackController < ApplicationController
   protect_from_forgery with: :null_session
 
   def index
+    render json: [], status: :ok
+  end
+
+  def line
+    ## ちゃんとLINEから来たリクエストか署名で検証する
     channel_secret = ENV["LINE_CHANNEL_SECRET"]
     http_request_body = request.raw_post
     hash = OpenSSL::HMAC::digest(OpenSSL::Digest::SHA256.new, channel_secret, http_request_body)
@@ -10,11 +15,61 @@ class CallbackController < ApplicationController
       render json: [], status: :ng
       return
     end
+
+    ## だれからのメッセージかとその内容
     from = params[:result][0][:content][:from]
     text = params[:result][0][:content][:text]
+
+    ## 翻訳結果をメッセージ
     translated = translate(text)
     line_messaging(from, translated)
     render json: [], status: :ok
+  end
+
+  def facebook
+
+    case request.request_method
+    when "GET"
+      render json: params["hub.challenge"], status: :ok
+
+    when "POST"
+      request_headers = {"Content-Type": "application/json"}
+      params[:entry].each do |ent|
+        ent[:messaging].each do |message|
+          res = JSON.parse(RestClient.get(URI.encode("https://api.photozou.jp/rest/search_public.json?keyword=#{message[:message][:text]}&limit=3")))
+          elements = []
+          res["info"]["photo"].each.with_index(1) do |photo, index|
+            elements << {
+              title: "「#{message[:message][:text]}」の検索結果#{index}",
+              image_url: photo["image_url"],
+              buttons: [
+                {
+                  type: "web_url",
+                  url: photo["url"],
+                  title: "View Web Page",
+                }
+              ]
+            }
+          end
+          message_data = {
+            attachment: {
+              type: "template",
+              payload: {
+                template_type: "generic",
+                elements: elements
+              }
+            }
+          }
+          request_params = {
+            recipient: {id: message[:sender][:id]},
+            message: message_data
+          }
+          RestClient.post "https://graph.facebook.com/v2.6/me/messages?access_token=#{ENV["SERVICE_SAFARI_PAGE_TOKEN"]}", request_params.to_json, request_headers
+        end
+      end
+      render json: [], status: :ok
+
+    end
   end
 
   private
@@ -28,8 +83,8 @@ class CallbackController < ApplicationController
       }
       request_params = {
         to: [to],
-        toChannel: 1383378250,
-        eventType: "138311608800106203",
+        toChannel: 1383378250, # この値はFIXらしいです
+        eventType: "138311608800106203", # この値はFIXらしいです
         content:{
           contentType: 1,
           toType: 1,
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root 'callback#index'
-  post "/callback" => "callback#index"
+  get "/facebook" => "callback#facebook"
+  post "/facebook" => "callback#facebook"
+  post "/line" => "callback#line"
 end
